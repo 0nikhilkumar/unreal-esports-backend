25e9ea251b3499e44bf7af4c5c592d38aa2cd748
Soul Predator <nnnnikhil5@gmail.com>
2025-02-03 18:37:39 +0530

    changes in app.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ const app = express();
 const httpServer = createServer(app);
 export const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:5173", "*"],
+    origin: ["http://localhost:5173", "*", "https://unrealesports-g6rtbxo4a-soulpredator0s-projects.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   },
